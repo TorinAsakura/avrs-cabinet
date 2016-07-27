bff5f30926800714bb323b8886cf0721c7f465bc
Andrew Ghostuhin <me@torinasakura.name>
2016-07-27 23:25:36 +0300

    [AVRS-13] Настройка и отладка окружения

---
+++ src/cabinet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const Root = () => (
+  <div>
+    Cabinet
+  </div>
+)
+
+render(
+  <Root />,
+  document.getElementById('container')
+)
